8781bf419b493ea27c820d320d4d990214ae25f4
Vineet Joshi <vineet0713@gmail.com>
2020-07-09 15:13:39 -0700

    Add routes for getting item data with support for pagination.

--- controllers/items.js
+++ controllers/items.js
@@ -9,3 +9,41 @@ exports.storeItem = (request, response, next) => {
 		.then(itemPost => response.status(201).json({ message: 'success!' }))
 		.catch(error => next(error));
 };
+
+exports.getItems = (request, response, next) => {
+	const pageSize = +request.query.pageSize;
+	const currentPage = +request.query.page;
+	const itemQuery = Item.find();
+	if (pageSize && currentPage) {
+		itemQuery
+			.sort({ createdAt: -1 })				// sorts items in descending order of creation time
+			.skip(pageSize * (currentPage - 1))		// skips the first n items
+			.limit(pageSize)						// only returns n items
+	}
+	let totalItems;
+	Item.find().countDocuments()
+		.then(itemCount => {
+			totalItems = itemCount;
+			return itemQuery;
+		})
+		.then(fetchedItems => {
+			response.status(200).json({
+				message: 'success!',
+				items: fetchedItems,
+				totalItems: totalItems,
+			});
+		})
+		.catch(error => next(error));
+};
+
+exports.getItem = (request, response, next) => {
+	const itemId = request.params.itemId;
+	Item.findById(itemId)
+		.then(fetchedItem => {
+			response.status(200).json({
+				message: 'success!',
+				item: fetchedItem,
+			});
+		})
+		.catch(error => next(error));
+};
--- routes/items.js
+++ routes/items.js
@@ -6,4 +6,10 @@ const itemsController = require('./../controllers/items');
 // POST api/item
 router.post('/item', itemsController.storeItem);
 
+// GET api/items
+router.get('/items', itemsController.getItems);
+
+// GET api/item
+router.get('/item/:itemId', itemsController.getItem);
+
 module.exports = router;
